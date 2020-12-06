ad752ac92fb14e37167899906b6528035ffe0193
Valarie Shek <valarieyuikei.shek21@ycdsbk12.ca>
2020-12-06 03:50:50 +0000

    problem 1 complete

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,5 +1,42 @@
 package recursionAssignment;
+import java.io.*;
 
 public class Recursion{
+  
+  public static int count8(int N){
+    int Num8;
+    int intCount = 0;
+    if(N > 1){
+      Num8 = N%10;
+      if(Num8 == 8){
+        intCount = intCount +1;
+      }
+      N = (N-Num8)/10;
+      return intCount + count8(N);
+    }else{
+    }return 0;
+  }
+  
+  /*public static String endX(String str){
+    String strWord;
+    String reverse;
+    if(strWord.equals(" ")){
+    return " ";
+    }else{
+    
+    }return reverse(strWord.substring(1) + strWord.substring(0));
 
+  }
+
+  /*public static String stringClean(String str){
+
+  }*/
+
+
+  public static void main(String[] args) throws IOException{
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    int N;
+    N = Integer.parseInt(keyboard.readLine());
+    System.out.println(Recursion.count8(N));
+  }
 }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -3,8 +3,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Before;
 import org.junit.jupiter.api.After;
+import java.io.*;
 
-import rename-package.Main;
+import recursionAssignment.Recursion;
 
 public class RecursionTest{
     
@@ -28,6 +29,16 @@ public class RecursionTest{
     @After
     public void afterTest(){
       // some steps to perform after tests are run
+    }
+
+    public static void main (String[] args) throws IOException{
+      BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+      String str;
+      String strWord;
+      str = keyboard.readLine();
+      strWord = Recursion.endX(str);
+      System.out.println(strWord);
+      
     }
     
     
